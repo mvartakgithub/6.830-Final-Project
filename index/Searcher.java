0a0e12891f6e6cba6ff7a1371c998c59510fd155
Abhishek Sarkar <aksarkar@mit.edu>
2012-04-25 12:42:22 -0400

    Initial version of Lucene indexing code
    Indexer.java walks a directory, parses all the XML files, and produces a Lucene
    index in the current directory.
    
    Searcher.java provides a (very) simple command-line interface to search the
    index: it just reads queries off of standard in and returns a list of (score,
    title) pairs.
    
    I'm using a Makefile here for simplicity (and because I don't know ant well
    enough to write a build.xml from scratch).

---
+++ index/Searcher.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.standard.*;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryParser.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.*;
+import org.apache.lucene.util.*;
+
+class Searcher {
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.err.println("Usage: java Searcher INDEX");
+      System.exit(1);
+    }
+    try {
+      IndexReader r = IndexReader.open(FSDirectory.open(new File(args[0])));
+      IndexSearcher s = new IndexSearcher(r);
+      Analyzer a = new StandardAnalyzer(Version.LUCENE_36);
+      QueryParser p = new QueryParser(Version.LUCENE_36, "fulltext", a);
+      Scanner in = new Scanner(System.in);
+      while (in.hasNextLine()) {
+        Query q = p.parse(in.nextLine().trim());
+        TopDocs hits = s.search(q, 10);
+        for (ScoreDoc d : hits.scoreDocs) {
+          Document doc = s.doc(d.doc);
+          float score = d.score;
+          String title = doc.getFieldable("title").stringValue();
+          System.out.format("%f %s\n", score, title);
+        }
+        System.out.println("Query finished");
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
