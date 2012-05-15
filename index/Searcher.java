7727e280a12c65a7fd90d267b1281f0513a7e7b1
Abhishek Sarkar <aksarkar@mit.edu>
2012-05-15 10:17:35 -0400

    Extend example to search multiple fields with variable boosting

--- index/Searcher.java
+++ index/Searcher.java
@@ -7,20 +7,28 @@ import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queryParser.*;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.function.*;
 import org.apache.lucene.store.*;
 import org.apache.lucene.util.*;
 
 class Searcher {
   public static void main(String[] args) {
-    if (args.length < 1) {
-      System.err.println("Usage: java Searcher INDEX");
+    if (args.length < 4) {
+      System.err.println("Usage: java Searcher INDEX TWEIGHT AWEIGHT FWEIGHT");
       System.exit(1);
     }
     try {
       IndexReader r = IndexReader.open(FSDirectory.open(new File(args[0])));
       IndexSearcher s = new IndexSearcher(r);
       Analyzer a = new StandardAnalyzer(Version.LUCENE_36);
-      QueryParser p = new QueryParser(Version.LUCENE_36, "fulltext", a);
+      String[] fields = new String[] {"title", "abstract", "fulltext"};
+      Map<String, Float> boosts = new HashMap<String, Float>();
+      boosts.put("title", Float.parseFloat(args[1]));
+      boosts.put("abstract", Float.parseFloat(args[2]));
+      boosts.put("fulltext", Float.parseFloat(args[3]));
+      QueryParser p = new MultiFieldQueryParser(Version.LUCENE_36, fields, a,
+                                                boosts);
+      ValueSourceQuery pagerank = new FieldScoreQuery("pagerank", FieldScoreQuery.Type.FLOAT);
       Scanner in = new Scanner(System.in);
       while (in.hasNextLine()) {
         Query q = p.parse(in.nextLine().trim());
